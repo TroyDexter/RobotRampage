dd8c73696ff07d5d01e80b45f6aaa1b79f1cdf41
Troy <troydexter5@gmail.com>
2021-10-19 19:08:06 -0400

    Chapter 9 - Added Gun Firing Animations

---
+++ Assets/Scripts/AssaultRifle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AssaultRifle : Gun
+{
+    override protected void Update()
+    {
+        base.Update();
+
+        //Automatic Fire
+        if (Input.GetMouseButton(0) && Time.time - lastFireTime > fireRate)
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
