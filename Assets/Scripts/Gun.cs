ef8fe38f46b75c353bc89f976a7ef4d7bfad5f6d
Troy <troydexter5@gmail.com>
2021-10-20 10:56:29 -0400

    Chapter 9 - Adding Gun Functions And Firing Sounds

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,9 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public float fireRate;
+    public Ammo ammo;
+    public AudioClip liveFire;
+    public AudioClip dryFire;
 
     protected float lastFireTime;
 
@@ -22,6 +26,15 @@ public class Gun : MonoBehaviour
 
     protected void Fire()
     {
+        if (ammo.HasAmmo(tag))
+        {
+            GetComponent<AudioSource>().PlayOneShot(liveFire);
+            ammo.ConsumeAmmo(tag);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(dryFire);
+        }
         GetComponentInChildren<Animator>().Play("Fire");
     }
 }
