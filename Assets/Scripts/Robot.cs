be61b4beb8e90bb8151acf5824eb58bc09c08bd9
Troy <troydexter5@gmail.com>
2021-11-02 15:28:21 -0400

    Chapter 10 - Added Robot movement animation and firing animation

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -18,6 +18,8 @@ public class Robot : MonoBehaviour
 
     private bool isDead;
 
+    public Animator robot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -52,6 +54,6 @@ public class Robot : MonoBehaviour
 
     private void fire()
     {
-        Debug.Log("Fire");
+        robot.Play("Fire");
     }
 }
